248be23d52b11add4d7d309b41a570028303a142
francesca2 <francesca.giordani88@gmail.com>
2016-11-08 12:12:40 +0100

    managed bean

---
+++ ManagedBean/src/main/java/ManagedBean/Cliente.java
@@ -0,0 +1,47 @@
+package ManagedBean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Cliente implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String nome;
+	private String cognome;
+
+	public Cliente() {
+	}
+
+	public Cliente(String nome, String cognome) {
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
