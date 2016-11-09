f9fcc07bbc3e533568a0b7d15aeb711a0a673378
francesca2 <francesca.giordani88@gmail.com>
2016-11-09 11:19:30 +0100

    redirect

--- Navigatore/src/main/java/it/alfasoft/francesca/Navigator.java
+++ Navigatore/src/main/java/it/alfasoft/francesca/Navigator.java
@@ -1,8 +1,11 @@
 package it.alfasoft.francesca;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name="n", eager=true)
 public class Navigator implements Serializable {
@@ -13,6 +16,7 @@ public class Navigator implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String pagina;
+	private String msg;
 	
 	public Navigator(){
 	 }
@@ -25,9 +29,33 @@ public class Navigator implements Serializable {
 		this.pagina = pagina;
 	}
 
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+	
+	public String nextPage(){
+		
+		if(pagina.isEmpty())
+		{
+			msg="Devi Scegliere una pagina!";
+			return "NavigationPage";
+		}
+		//per cambiare l'url quando vengo reinderezzata alla pagina
+		return pagina + "?faces-redirect=true";
+	}
 
+	public void redirectPage() throws IOException{
+		
+		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+		context.redirect(context.getRequestContextPath() + "/paginaUno.xhtml");
+	}
 	 
 }
